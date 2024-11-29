61c5dc60154e14e3e53ef1bd9033fd41346d240d
tiwang-ai <setup@tiwang.com.br>
2024-11-29 16:56:10 -0300

    ajuste pacotes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended'
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { 
+    '@typescript-eslint/no-unused-vars': ['warn', { 
       argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_'
-    }]
+      varsIgnorePattern: '^_',
+      ignoreRestSiblings: true 
+    }],
+    'no-unused-vars': 'off'
   }
-}
+};
