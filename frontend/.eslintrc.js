5d832e31cf3184f0a62776094e8422b98e30b3f4
tiwang-ai <setup@tiwang.com.br>
2024-11-30 13:54:11 -0300

    ajuste para outra tentativa de deploy

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,13 +5,19 @@ module.exports = {
     'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { 
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_',
-      ignoreRestSiblings: true 
-    }],
-    'no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { 
+        vars: 'all', 
+        varsIgnorePattern: '^_',
+        args: 'after-used', 
+        argsIgnorePattern: '^_' 
+      }
+    ],
+    'react/react-in-jsx-scope': 'off' // Para React 17+
   }
 };
